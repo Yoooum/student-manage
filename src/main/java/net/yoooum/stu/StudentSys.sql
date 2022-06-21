27ad732712cbe9b693b49dfc9617e904d14029c3
phj233 <2780990934@qq.com>
2022-06-21 15:44:54 +0800

    添加用户数据

--- src/main/java/net/yoooum/stu/StudentSys.sql
+++ src/main/java/net/yoooum/stu/StudentSys.sql
@@ -24,10 +24,24 @@ create table if not exists tb_course
 );
 
 
-insert into tb_user values (1, 'admin', 'admin');
+insert into tb_user values (1, 'admin', 'admin'),
+                           (2,'phj233','phj123456');
 select * from tb_user;
 
-insert into tb_student values (10,'嘿嘿嘿',99,'男');
+insert into tb_student values(01,'赵雷',11,'男'),
+                           (02,'钱电',12,'女'),
+                           (03,'孙风',19,'男'),
+                           (04,'李云',20,'男'),
+                           (05,'周梅',21,'女'),
+                           (06,'吴兰',11,'男'),
+                           (07,'郑竹',81,'女'),
+                           (08,'张三',12,'男'),
+                           (09,'李四',25,'男'),
+                           (10,'王五',06,'男'),
+                           (11,'赵六',13,'女'),
+                           (12,'孙七',01,'女'),
+                           (13,'王二麻子',85,'男'),
+                           (14,'嘿嘿嘿',99,'男');
 select * from tb_student;
 UPDATE tb_student SET age=9 , sex='女' WHERE sid=1000000;
 insert into tb_course values (1,'Linux操作系统'),(2,'Java程序设计'),(3,'计算机网络'),(4,'数据库技术'),(5,'大学英语Ⅱ');
