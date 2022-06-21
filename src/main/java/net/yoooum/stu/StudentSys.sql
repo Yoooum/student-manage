2d7552ac35766c591c1e683601cc9f3d1fc51c82
Yoooum <akyomi@qq.com>
2022-06-21 15:22:57 +0800

    update

--- src/main/java/net/yoooum/stu/StudentSys.sql
+++ src/main/java/net/yoooum/stu/StudentSys.sql
@@ -27,7 +27,7 @@ create table if not exists tb_course
 insert into tb_user values (1, 'admin', 'admin');
 select * from tb_user;
 
-insert into tb_student values (1000000,'嘿嘿嘿',99,'男');
+insert into tb_student values (10,'嘿嘿嘿',99,'男');
 select * from tb_student;
 UPDATE tb_student SET age=9 , sex='女' WHERE sid=1000000;
 insert into tb_course values (1,'Linux操作系统'),(2,'Java程序设计'),(3,'计算机网络'),(4,'数据库技术'),(5,'大学英语Ⅱ');
